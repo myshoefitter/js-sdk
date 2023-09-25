05a47de11a880f83a68ec979c31d0ccf5466d2fc
Marius Bolik <marius@eyloo.com>
2023-09-25 10:29:58 +0200

    chore(analytics): add pirsch tracking

--- src/script.ts
+++ src/script.ts
@@ -2,8 +2,12 @@
  * Represents a service with functionalities related to a product.
  */
 class MyShoefitter {
+  // The config to initialize the script
   private config: ScriptConfig | null = null;
+  // The dialog window
   private dialog: HTMLDialogElement | null = null;
+  // The params for the banner
+  private params: BannerParams | null = null;
 
   /**
    * Initialize the Script
@@ -12,28 +16,21 @@ class MyShoefitter {
   public init(config: ScriptConfig): void {
     this.config = config;
 
-    if (this.config?.productId) {
+    if (config?.productId) {
+
+      this.params = Object.assign(config,
+        {
+          sessionId: this.generateSessionId(),
+          ref: location.href // Don't remove or encrypt! Needed for Analytics!
+        });
+
       this.addButtonClickListener();
-      // this.trackScriptLoad();
-      console.log(`mySHOEFITTER Config: ${this.config}`);
+      this.trackEvent('Button Load');
+
+      console.log('mySHOEFITTER Config:', config);
+      console.log('mySHOEFITTER Session ID:', this.params.sessionId);
     } else {
       console.warn('mySHOEFITTER: productId is missing!');
-    }
-  }
-
-  /**
-   * Reloads or refreshes based on the current product ID.
-   * Will not perform any action if the product ID is not set.
-   */
-  public reload(): void {
-    if (this.config) {
-      // Your reloading logic here...
-      // @Ahmad wir brauchen diese funktion wahrscheinlich nicht mehr.
-      console.log('Reloaded mySHOEFITTER Config');
-    } else {
-      console.warn(
-        'mySHOEFITTER is not initialized. Calling reload() has no effect.',
-      );
     }
   }
 
@@ -41,7 +38,7 @@ class MyShoefitter {
     // Create the dialog element
     const dialog = document.createElement('dialog');
     dialog.style.width = '80%';
-    dialog.style.height = '300px';
+    dialog.style.height = '400px';
     dialog.style.padding = '0';
     dialog.style.border = 'none';
     dialog.style.borderRadius = '20px';
@@ -70,7 +67,8 @@ class MyShoefitter {
 
   public closeBanner(): void {
     if (this.config) {
-      return this.dialog?.close();
+      this.dialog?.close();
+      this.trackEvent('Banner Close');
     } else {
       console.warn('mySHOEFITTER is not initialized');
     }
@@ -85,11 +83,7 @@ class MyShoefitter {
       console.warn('mySHOEFITTER: No productId found!')
       return '';
     }
-    const params: Record<string, string> = {
-      productId: this.config.productId,
-      ref: window.location.href
-    };    
-    return 'https://banner.myshoefitter.com/?' + new URLSearchParams(params).toString();
+    return 'https://banner.myshoefitter.com/?' + new URLSearchParams(this.params as unknown as Record<string, string>).toString();
   }
 
   /**
@@ -98,7 +92,10 @@ class MyShoefitter {
   private addButtonClickListener(): void {
     const button = document.getElementById('myshoefitter-button');
     if (button) {
-      button.addEventListener('click', () => this.showBanner());
+      button.addEventListener('click', () => {
+        this.showBanner();
+        this.trackEvent('Banner Open');
+      });
     }
   }
 
@@ -116,13 +113,65 @@ class MyShoefitter {
         break;
     }
   }
+
+  /**
+   * Track script load event in Pirsch
+   * Extracted from https://api.pirsch.io/pirsch-events.js
+   */
+  private trackEvent(eventName: string) {
+
+    // Don't send request on localhost
+    if ((/^localhost(.*)$|^127(\.[0-9]{1,3}){3}$/is.test(location.hostname) || location.protocol === "file:")) {
+      console.info("Pirsch is ignored on localhost. Add the data-dev attribute to enable it.");
+      return true;
+    }
+
+    const data = {
+      identification_code: 'kGhjVS9A2aJtLg6PWZx0h6OV8N23WqEy',
+      url: 'https://banner.myshoefitter.com/',
+      title: document.title,
+      referrer: encodeURIComponent(location.href),
+      screen_width: screen.width,
+      screen_height: screen.height,
+      user_agent: navigator.userAgent,
+      event_name: eventName,
+      event_duration: 0,
+      event_meta: this.params
+    };
+
+    return navigator.sendBeacon('https://api.pirsch.io/event', JSON.stringify(data));
+  }
+
+  /**
+   * Generate a unique session ID
+   * @returns Session ID
+   * @see https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+   */
+  private generateSessionId() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (parseInt(c, 10) ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> parseInt(c, 10) / 4).toString(16)
+    );
+  }
 }
 
 interface ScriptConfig {
+  shopId: string;
   productId: string;
   logsEnabled?: boolean;
   buttonSelector?: string;
 }
 
-// Expose Class
-const myshoefitter = new MyShoefitter();
+interface BannerParams extends ScriptConfig {
+  sessionId: string;
+}
+
+interface MyShoefitterClass {
+  init: (config: ScriptConfig) => void;
+}
+
+interface WindowExtended extends Window {
+  myshoefitter: MyShoefitterClass;
+}
+
+// Expose class to parent page
+(window as unknown as WindowExtended).myshoefitter = new MyShoefitter();
