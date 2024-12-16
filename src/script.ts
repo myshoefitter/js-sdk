91bd1bce4283ef849d80dc6eca0ba583308bac37
Marius Bolik <marius@eyloo.com>
2024-12-16 13:14:07 +0100

    chore(button): change default button text

--- src/script.ts
+++ src/script.ts
@@ -420,7 +420,7 @@ class MyShoefitter {
   
       // Set properties on the new button
       mysfButton.id = 'myshoefitter-button'; // Set the button id
-      mysfButton.innerHTML = this.config?.button?.text || `Find size with <img src="https://cdn.myshoefitter.com/images/logo.png" style="height: 18px; margin: 0 0 -4px 4px;" />`; // Set the button text
+      mysfButton.innerHTML = this.config?.button?.text || `1 Foto - immer die perfekte Größe <img src="https://cdn.myshoefitter.com/images/logo.png" style="height: 18px; margin: 0 0 -4px 4px;" />`; // Set the button text
       mysfButton.type = 'button'; // Set the button type
 
       let styles: Partial<CSSStyleDeclaration> = {
